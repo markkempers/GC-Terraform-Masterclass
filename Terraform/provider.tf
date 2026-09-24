265d1c42f5f1dc017dce2476e019790467afe907
Mark Kempers <info@markkempers.nl>
2026-09-24 09:53:03 +0200

    Change statefile location

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "9.0.0"
     }
   }
+
+  backend "local" {
+    # Relative to this directory (Terraform/), so it resolves outside the
+    # git repo for every student regardless of where they cloned it.
+    path = "/root/.oci/terraform.tfstate"
+  }
 }
 
 provider "oci" {
